88f6d8baf03e2aa5e64da9d370755342e87a806f
Kevin Nguyen <KevinNguyen09161995@gmail.com>
2023-12-14 13:18:57 -0600

    Adjusted POST and added Detail View

--- market_api/urls.py
+++ market_api/urls.py
@@ -5,7 +5,7 @@ from .views import (MeleeWeaponView, WeaponDetailView, MagicWeaponView, RangedWe
 urlpatterns = [
     path('api/weapons/', AllWeaponView.as_view()),
     path('api/weapons/melee/', MeleeWeaponView.as_view()),
-    path('api/weapons/detail/<str:unique_name>/', WeaponDetailView.as_view()),
+    path('api/weapons/detail/<str:unique_name>', WeaponDetailView.as_view()),
     path('api/weapons/magic/', MagicWeaponView.as_view()),
     path('api/weapons/ranged/', RangedWeaponView.as_view()),
     path('api/equipments/offhand/', OffHandView.as_view()),
--- market_prices/views.py
+++ market_prices/views.py
@@ -1,6 +1,6 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
+from django.db.models import Q
 from .models import MarketPricesModel
 from .serializers import MarketPricesSerializer
 from datetime import datetime, timedelta
@@ -10,14 +10,16 @@ class MarketPricesDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MarketPricesModel.objects.all()
     serializer_class = MarketPricesSerializer
     lookup_field = 'ItemTypeId'
+
 class MarketPricesView(generics.ListAPIView):
     queryset = MarketPricesModel.objects.all()
     serializer_class = MarketPricesSerializer
 
     def post(self, request, *args, **kwargs):
-        # Try to get an existing record based on ItemTypeId
+        # Try to get an existing record based on ItemTypeId and QualityLevel
         item_type_id = request.data.get('ItemTypeId')
-        existing_instance = MarketPricesModel.objects.filter(ItemTypeId=item_type_id).first()
+        quality_level = request.data.get('QualityLevel')
+        existing_instance = MarketPricesModel.objects.filter(Q(ItemTypeId=item_type_id) & Q(QualityLevel=quality_level)).first()
 
         if existing_instance:
             # If the record already exists, update it
@@ -32,25 +34,26 @@ class MarketPricesView(generics.ListAPIView):
                 current_time = datetime.now()
                 if new_timestamp and (new_timestamp <= current_time and new_timestamp >= current_time - timedelta(hours=1)):
                     # Check if the new_unit_price is less than what is currently in the database
-                    current_max_unit_price = MarketPricesModel.objects.aggregate(Max('UnitPriceSilver'))['UnitPriceSilver__max']
+                    current_max_unit_price = MarketPricesModel.objects.filter(Q(ItemTypeId=item_type_id) & Q(QualityLevel=quality_level)).aggregate(Max('UnitPriceSilver'))['UnitPriceSilver__max']
                     if current_max_unit_price is not None and new_unit_price < current_max_unit_price:
                         # Update the existing model
                         serializer.save()
                         return Response(serializer.data, status=status.HTTP_200_OK)
+
                     else:
-                        return Response({"error": "New unit price must be less than the current maximum unit price."}, status=status.HTTP_400_BAD_REQUEST)
+                        return Response(status=status.HTTP_400_BAD_REQUEST)
 
                 else:
-                    return Response({"error": "New timestamp must be over 1 hour older than the current time."},
-                                    status=status.HTTP_400_BAD_REQUEST)
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         else:
             # If the record doesn't exist, create a new one
             serializer = self.get_serializer(data=request.data)
+
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-            
+
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
